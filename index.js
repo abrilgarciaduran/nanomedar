1017389dc0c3ba8e86d7c883fe0104174ec4e7cf
abrilgarciaduran <abrilgarciaduran@gmail.com>
2020-11-06 17:06:23 -0300

    NavBar Mobile

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const imgLogo = document.querySelector('#img-logo-main');
 const header = document.querySelector('header');
-const menu = document.querySelector('open-close-menu');
+const menu = document.querySelector('.links');
+const openCloseMenuContainer = document.querySelector('.open-close-menu-container');
+const openCloseMenuImg = document.querySelector('.open-close-menu');
 
 const checkScroll = () => {
     if (window.scrollY > 250) {
@@ -11,4 +13,14 @@ const checkScroll = () => {
         header.style.height = '80px';
     }
 }
-window.addEventListener('scroll', checkScroll)
+const displayMenu = () => {
+    if (openCloseMenuImg.src.includes('open')) {
+        openCloseMenuImg.src = openCloseMenuImg.src.replace('open', 'close')
+        menu.style.display = 'flex';
+    } else {
+        openCloseMenuImg.src = openCloseMenuImg.src.replace('close', 'open')
+        menu.style.display = 'none';
+    }
+}
+window.addEventListener('scroll', checkScroll);
+openCloseMenuContainer.addEventListener('click', displayMenu)
